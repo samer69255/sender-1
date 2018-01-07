e42b33098cc4e7c6e224feeb08c3e6b5022ed561
samer69255 <samer69255@gmail.com>
2018-01-07 19:38:37 +0300

    make it better

---
+++ server2.js
@@ -0,0 +1,131 @@
+//===========
+
+//==================
+
+//==============
+var express = require('express');
+var app = express();
+var nodemailer = require('nodemailer');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var run = false;
+var n = 0;
+var f = 0;
+io.on('connection', function(socket){
+
+    if (run == true)
+    {
+        socket.emit('disabled');
+        console.log(run);
+    }
+
+    socket.on('disconnect',function () {
+        console.log('user is disconnet')
+    });
+
+    socket.on('set',function (data) {
+        var fs = require('fs');
+        fs.writeFile('list.txt',data,function (err) {
+            if (err) console.log(err);
+            socket.emit('success');
+        });
+    });
+
+
+    socket.on('start',function (user) {
+        run = true;
+        user = JSON.parse(user);
+        socket.emit('disabled');
+        var fs = require('fs');
+        fs.readFile('list.txt',function (error,data) {
+
+            data = data.toString();
+             list = data.trim().split('\n');
+            f = list.length;
+
+
+
+             mailOptions = {
+                from: user.from,
+                to:null,
+                subject: user.subject,
+                html: user.code
+            };
+
+            sendemail(list[0]);
+
+        });
+    });
+
+
+
+
+    console.log('user is connected');
+});
+server.listen(3000);
+app.use(express.static('public'));
+
+//================
+
+
+//==================
+app.get('/',function (req,res) {
+
+    res.sendFile( __dirname + '/index.html');
+});
+
+//==============
+
+
+
+
+
+var transport = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 587,
+    auth: {
+        user: "apikey",
+        pass: "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"
+    }
+});
+
+
+
+
+
+function sendemail(to) {
+
+    console.log(n);
+    var data = {
+        n:n+1,
+        f:f,
+        to:to
+    }
+
+    io.sockets.emit("status",JSON.stringify(data));
+
+    mailOptions.to = to;
+    console.log('sending to ' + JSON.stringify(mailOptions));
+
+    transport.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            n++;
+            if (n == f)
+            {
+                    console.log('success');
+                    io.sockets.emit('endabled',n);
+                    run = false;
+                    n = 0;
+                    f = 0;
+            }
+            else
+            {
+                sendemail(list[n]);
+
+            }
+        }
+    });
+}
